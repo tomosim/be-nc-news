7da8285a76616b0ebf9b9d6241ee8d6a2a4e82ff
Tom Simmons <tomsimmons@toms-mbp-2.home>
2020-03-27 11:16:53 +0000

    add users schema

--- db/migrations/20200327105611_users.js
+++ db/migrations/20200327105611_users.js
@@ -1,8 +1,11 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("users", table => {
+    table.string("username").primary();
+    table.string("name");
+    table.string("avatar_url");
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("users");
 };
