36413ceb2d5eec85ac5e0d9dd7cb0781de7212fb
Tom Simmons <tomsimmons@toms-mbp-2.home>
2020-03-27 11:12:51 +0000

    add topics schema

--- db/migrations/20200327105603_topics.js
+++ db/migrations/20200327105603_topics.js
@@ -1,8 +1,11 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("topics", table => {
+    table.increments("id");
+    table.string("description");
+    table.string("slug");
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("topics");
 };
