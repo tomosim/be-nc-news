8e2fc6a2bf2c7a294d98f4e974683b625f61e817
Tom Simmons <tomsimmons@toms-mbp-2.home>
2020-03-30 10:23:29 +0100

    add comments schema

--- db/migrations/20200327105631_comments.js
+++ db/migrations/20200327105631_comments.js
@@ -1,8 +1,20 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("comments", table => {
+    table.increments("comment_id").primary();
+    table.string("body");
+    table
+      .integer("article_id")
+      .references("article_id")
+      .inTable("articles");
+    table
+      .string("username")
+      .references("username")
+      .inTable("users");
+    table.integer("votes").defaultsTo(0);
+    table.timestamp("created_at").defaultsTo(knex.fn.now(6));
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("comments");
 };
