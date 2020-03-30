e62abe232c47bb73f67836103b61738497956614
Tom Simmons <tomsimmons@toms-mbp-2.home>
2020-03-30 11:50:26 +0100

    create formatDates uitls function

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -7,7 +7,14 @@ exports.formatUsers = list => {
   return formattedList;
 };
 
-exports.formatDates = list => {};
+exports.formatDates = list => {
+  const formattedList = list.map(item => {
+    const { created_at, ...remainingKeys } = item;
+    return { created_at: new Date(created_at), ...remainingKeys };
+  });
+
+  return formattedList;
+};
 
 exports.makeRefObj = list => {};
 
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -87,7 +87,46 @@ describe("formatUsers", () => {
   });
 });
 
-describe("formatDates", () => {});
+describe("formatDates", () => {
+  it("returns an empty array when given an empty array", () => {
+    expect(formatDates([])).to.deep.equal([]);
+  });
+  it("returns an array of one object with the created_at value converted into a date object ", () => {
+    const articles = [{ created_at: 1037708514171 }];
+    const formattedArticles = [{ created_at: new Date(1037708514171) }];
+    expect(formatDates(articles)).to.deep.equal(formattedArticles);
+  });
+  it("returns and array of one object with a copy of all keys that aren't created_at unchanged", () => {
+    const articles = [{ created_at: 1037708514171, title: "test article" }];
+    const formattedArticles = [
+      { created_at: new Date(1037708514171), title: "test article" }
+    ];
+    expect(formatDates(articles)).to.deep.equal(formattedArticles);
+  });
+  it("returns an array of multiple objects each with the created_at key formatted", () => {
+    const articles = [
+      { created_at: 1037708514171, title: "test article 1" },
+      { created_at: 1037708514172, title: "test article 2" }
+    ];
+    const formattedArticles = [
+      { created_at: new Date(1037708514171), title: "test article 1" },
+      { created_at: new Date(1037708514172), title: "test article 2" }
+    ];
+    expect(formatDates(articles)).to.deep.equal(formattedArticles);
+  });
+  it("doesn't mutate the original array", () => {
+    const articles = [
+      { created_at: 1037708514171, title: "test article 1" },
+      { created_at: 1037708514172, title: "test article 2" }
+    ];
+    const copiedArticles = [
+      { created_at: 1037708514171, title: "test article 1" },
+      { created_at: 1037708514172, title: "test article 2" }
+    ];
+    formatDates(articles);
+    expect(articles).to.deep.equal(copiedArticles);
+  });
+});
 
 describe("makeRefObj", () => {});
 
