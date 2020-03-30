b991b188295ae8067b82c159af6f39c8df4f9d32
Tom Simmons <tomsimmons@toms-mbp-2.home>
2020-03-30 10:28:12 +0100

    add likes schema

--- db/migrations/20200327105715_likes.js
+++ db/migrations/20200327105715_likes.js
@@ -1,8 +1,16 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("likes", table => {
+    table
+      .string("username")
+      .references("username")
+      .inTable("users");
+    table
+      .integer("article_id")
+      .references("article_id")
+      .inTable("articles");
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("likes");
 };
