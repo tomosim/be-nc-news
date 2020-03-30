02b8017c46d269822fb8c349b1122146b3ae56fd
Tom Simmons <tomsimmons@toms-mbp-2.home>
2020-03-30 10:17:58 +0100

    add articles schema

--- db/migrations/20200327105625_articles.js
+++ db/migrations/20200327105625_articles.js
@@ -1,8 +1,20 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("articles", table => {
+    table.increments("article_id");
+    table.string("body");
+    table
+      .string("author")
+      .references("username")
+      .inTable("users");
+    table
+      .string("topic")
+      .references("slug")
+      .inTable("topics");
+    table.integer("votes").defaultsTo(0);
+    table.timestamp("created_at").defaultsTo(knex.fn.now());
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("articles");
 };
