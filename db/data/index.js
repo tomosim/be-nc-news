cfc221a50d62a2bae7cd9b915ed86eb707935bc9
Tom Simmons <tomsimmons@toms-mbp-2.home>
2020-03-30 10:55:02 +0100

    correctly export dev data

--- db/data/index.js
+++ db/data/index.js
@@ -2,7 +2,7 @@ const ENV = process.env.NODE_ENV || "development";
 
 const data = {
   test: require("./test-data"),
-  development: require("./test-data")
+  development: require("./development-data")
 };
 
 module.exports = data[ENV];
