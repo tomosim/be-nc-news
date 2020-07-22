8f176ac8b96b2d1c2ca415ca63ad77fc0c658895
Tom Simmons <tom_simmons@live.co.uk>
2020-07-22 10:15:32 +0100

    tidy up knex file and seed docker db

--- db/data/index.js
+++ db/data/index.js
@@ -4,6 +4,7 @@ const data = {
   test: require("./test-data"),
   development: require("./development-data"),
   production: require("./development-data"),
+  docker: require("./development-data"),
 };
 
 module.exports = data[ENV];
--- knexfile.js
+++ knexfile.js
@@ -1,36 +1,31 @@
-// const ENV = process.env.NODE_ENV || "development";
-// const { DB_URL } = process.env;
+const ENV = process.env.NODE_ENV || "development";
+const { DB_URL } = process.env;
 
-// const baseConfig = {
-//   client: "pg",
-//   migrations: {
-//     directory: "./db/migrations",
-//   },
-//   seeds: {
-//     directory: "./db/seeds",
-//   },
-// };
-
-// const customConfig = {
-//   development: {
-//     connection: process.env.DATABASE_URL,
-//   },
-//   test: {
-//     connection: {
-//       database: "nc_news_test",
-//     },
-//   },
-//   production: {
-//     connection: `${DB_URL}?ssl=true`,
-//   },
-// };
-
-// console.log(customConfig[ENV]);
-// module.exports = { ...customConfig[ENV], ...baseConfig };
-console.log(process.env.DATABASE_URL);
-module.exports = {
+const baseConfig = {
   client: "pg",
-  migrations: { directory: "./db/migrations" },
-  seeds: { directory: "./db/seeds" },
-  connection: process.env.DATABASE_URL,
+  migrations: {
+    directory: "./db/migrations",
+  },
+  seeds: {
+    directory: "./db/seeds",
+  },
 };
+
+const customConfig = {
+  development: {
+    connection: "nc_news",
+  },
+  test: {
+    connection: {
+      database: "nc_news_test",
+    },
+  },
+  production: {
+    connection: `${DB_URL}?ssl=true`,
+  },
+  docker: {
+    connection: process.env.DATABASE_URL,
+  },
+};
+
+module.exports = { ...customConfig[ENV], ...baseConfig };
