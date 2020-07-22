da7be175e2a508547ba4f16236dd213bebcdf6ca
Tom Simmons <tom_simmons@live.co.uk>
2020-07-22 12:52:07 +0100

    add endpoints endpoint

--- routes/api.router.js
+++ routes/api.router.js
@@ -1,8 +1,12 @@
 const apiRouter = require("express").Router();
-const articleRouter = require("./article.router");
+const articleRouter = require("./article.rxouter");
 const commentRouter = require("./comment.router");
 const topicRouter = require("./topic.router");
 const userRouter = require("./user.router");
+
+const endpoints = require("../endpoints.json");
+
+apiRouter.get("/", endpoints);
 
 apiRouter.use("/articles", articleRouter);
 apiRouter.use("/comments", commentRouter);
