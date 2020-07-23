42231940f7fcc7b7979eed674077b38aed74bec5
Tom Simmons <tom_simmons@live.co.uk>
2020-07-23 11:51:03 +0100

    fix typo

--- routes/api.router.js
+++ routes/api.router.js
@@ -1,5 +1,5 @@
 const apiRouter = require("express").Router();
-const articleRouter = require("./article.rxouter");
+const articleRouter = require("./article.router");
 const commentRouter = require("./comment.router");
 const topicRouter = require("./topic.router");
 const userRouter = require("./user.router");
