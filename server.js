4c7169e996a848363901bbada09a6fb335c48c67
Tom Simmons <tom_simmons@live.co.uk>
2020-07-23 11:05:30 +0100

    add cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const apiRouter = require("./routes/api.router");
 const {
   handleCustomErrors,
@@ -7,6 +8,8 @@ const {
 } = require("./errors");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
